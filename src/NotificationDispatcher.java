0dc7cc5bf80c5cd5f0f18ea5065c1c23127a5fef
Ziafat-Hussain <hussainziafat591@gmail.com>
2026-06-01 22:28:47 +0500

    Delete src/NotificationDispatcher.java

--- src/NotificationDispatcher.java
+++
@@ -1,7 +0,0 @@
-package events;
-// PATTERN: Observer
-// RATIONALE: Notifications are dispatched when relevant events are published.
-public class NotificationDispatcher implements Observer {
-    public void update(DomainEvent event) { System.out.println("[NotificationDispatcher] Notification sent for event: " + event.getEventType()); }
-    public String getObserverId() { return "NotificationDispatcher"; }
-}
