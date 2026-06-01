3b842c09ccf0bccfa9e72194cd4c6225557cb987
Ziafat-Hussain <hussainziafat591@gmail.com>
2026-06-01 22:25:58 +0500

    Delete src/MetricsCollector.java

--- src/MetricsCollector.java
+++
@@ -1,7 +0,0 @@
-package events;
-// PATTERN: Observer
-// RATIONALE: Metrics are updated independently whenever events are published.
-public class MetricsCollector implements Observer {
-    public void update(DomainEvent event) { System.out.println("[MetricsCollector] Metrics updated for: " + event.getEventType()); }
-    public String getObserverId() { return "MetricsCollector"; }
-}
